139dc3b309f69e38f7ff86273f7ec85454947c90
Meohee <dulgi7245@scg.skku.ac.kr>
2020-12-02 22:19:23 +0900

    react

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+           Read! <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
+        </p> 
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
